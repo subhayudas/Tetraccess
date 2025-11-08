044af619b17fbc75c99f168b6133370ebe61a587
subhayudas <subhayudas49@gmail.com>
2025-11-08 22:58:42 +0530

    scopes

--- lib/google-auth.ts
+++ lib/google-auth.ts
@@ -43,6 +43,7 @@ export function getGoogleAuthUrl() {
     scope: [
       'https://www.googleapis.com/auth/userinfo.email',
       'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/gmail.modify',
     ].join(' '),
   }
 
